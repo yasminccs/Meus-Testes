24a10f7a8226332793834541ce1fb397076f9572
Yasmin Cibreiros Chagas <114839561+yasminccs@users.noreply.github.com>
2022-10-08 20:09:44 -0300

    save

--- Funções/Exercício 1/script.js
+++ Funções/Exercício 1/script.js
@@ -3,22 +3,20 @@ const arr = []
 function addNumber(){
     const nmr = document.querySelector("#nmr")
     const input = Number(nmr.value)
-    arr.push(input)
-    let indc = arr.includes(input)
-    if(indc){
+    const indc = arr.includes(input)
+    if(input === 0 || input < 0 || input > 100 || indc){
         alert('Valor inválido ou já encontrado na lista.')
     } 
     else {
+        arr.push(input)
         const res = document.querySelector('#res')
         const options = document.createElement('option')
         options.innerHTML += `Valor ${input} adicionado.`
         res.appendChild(options)
     }
-
-    console.log(arr)
-    console.log(indc)
 }
 
 function final(){
-
+    const result = document.querySelector('#result')
+    result.innerHTML = `Ao todo, temos ${arr.length} números cadastrados.`
 }
